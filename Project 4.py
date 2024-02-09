1b4bec6b8176a7afb03d658c12e61aad52965f51
antara44 <156001484+antara44@users.noreply.github.com>
2024-02-10 00:00:35 +0530

    Update Project 4.py

--- Project 4.py
+++ Project 4.py
@@ -8,44 +8,42 @@ class project1:
  def Haversine(self,latp,lat_target,lonp,lon_target): #calculating distance between two points using haversine formula
      
     latp = (latp) * math.pi / 180.0 #converting all coordinates in degrees to radians
+  
     lat_target = (lat_target) * math.pi / 180.0
+  
     lonp = (lonp) * math.pi / 180.0
+  
     lon_target = (lon_target) * math.pi / 180.0
 
     a=pow(math.sin((lat_target-latp)/2),2)
+  
     b=math.cos(lat_target)*math.cos(latp)*pow(math.sin((lon_target-lonp)/2),2)
+  
     self.dist=2*self.r*math.asin(math.sqrt(a+b))#in kilometers
+  
     theta=self.dist/self.r
 
     print(self.dist)#distance in kilometers
     print(theta)#central angle in radians
- '''def Trajectory(self,theta,vel_x,dist):
-
-   print(math.sin(theta/2))
-   b=(2*self.r*self.g*math.sin(theta/2))/(pow(vel_x,2))
-   theta_dash=0.5*math.asin(b)
-   print (theta_dash)
-   for i in range(0,dist,+10):
-    x=i
-    if(theta_dash<math.pi//2):
-     y=x*math.tan(theta_dash)-self.g*pow(x,2)/(2*pow(vel_x,2)*pow(math.cos(theta_dash),2))
-     print(y)'''
- def Trajectory(self,vel_x,total_weight,payload):
+ 
+ def Trajectory(self,v):
    H=20
-   u=vel_x
+  
+   u=v#obtained from Drag Function
+  
    D=math.sqrt(2*u*u*H/self.g)#horizontal range of the payload
+  
    dist_travel=self.dist-D
+  
    print(D)
-   '''for i in range(1,math.floor(D),+5):#generates a parabolic trajectory
-     x=i
-     y=self.g*x*x/(2*u*u)
-     print(y)'''
+   
    for i in range(0,math.floor(self.dist)):
+    
      if(i==math.floor(dist_travel)):
        print("payload is dropped at",dist_travel)
-       total_weight=total_weight-payload
-       vel_x=0# drone must stop
+       
  def Drag(self,density,area,drag_coeff,drag_force,wind_vel):
+  
    v1=math.sqrt(2*drag_force/(density*drag_coeff*area))
    v=v1+wind_vel #velocity of uav wrt ground=velocity of uav wrt medium+ velocity of medium wrt ground
    print(v)  
